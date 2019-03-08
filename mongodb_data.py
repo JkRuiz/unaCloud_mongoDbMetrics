f1ee24ba09d70a0bf12deeaacce73825ae125e42
Juan Camilo Ruiz <jc.ruiz@uniandes.edu.co>
2019-03-08 10:26:43 -0500

    Mongodb_data more organized

--- mongodb_data.py
+++ mongodb_data.py
@@ -4,6 +4,9 @@ import datetime
 import pprint
 
 columnsDelete = ['_id', 'ram', 'swap', 'disk', 'net_io_counters', 'vms', 'virtualbox_status', 'vbox_process_count', 'unacloud_status', 'rtt']
+# Dates with format Year,Month,Day,Hour,Minuts
+startDate = [2019, 3, 1, 0, 0]
+limitDate = [2019, 4, 1, 0, 0]
 
 # Connect to the mongo DB
 client = MongoClient('mongodb://157.253.205.96', 27017)
@@ -12,10 +15,10 @@ db = client.performance_collector_unacloud
 # Get the specific collection
 collection = db.MetricsCollection
 # Set the times in wich the metrics will be obtain
-startDate = datetime.datetime(2019, 3, 1, 0, 0).strftime("%Y-%m-%dT%H:%M:%S")
-limitDate = datetime.datetime(2019, 4, 1, 0, 0).strftime("%Y-%m-%dT%H:%M:%S")
+startTotalDate = datetime.datetime(startDate[0], startDate[1], startDate[2], startDate[3], startDate[4]).strftime("%Y-%m-%dT%H:%M:%S")
+limitTotalDate = datetime.datetime(limitDate[0], limitDate[1], limitDate[2], limitDate[3], limitDate[4]).strftime("%Y-%m-%dT%H:%M:%S")
 # Get the metrics from the DB
-metrics = collection.find({"$and": [{"timestamp": {"$gt": startDate}}, {"timestamp": {"$lt": limitDate}}]})
+metrics = collection.find({"$and": [{"timestamp": {"$gt": startTotalDate}}, {"timestamp": {"$lt": limitTotalDate}}]})
 # Create a dataframe of pandas with those metrics
 df = pd.DataFrame(list(metrics))
 # Delete the columns that won't be use
